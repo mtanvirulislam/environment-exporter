43c42e257520ee0f97fb652775f24749a294da47
Mohammed Tanvirul Islam <mtanvirul@keedio.com>
2019-05-16 11:07:26 +0200

    Export txt, xls and csv

--- user-env-script.sh
+++ user-env-script.sh
@@ -1,35 +1,66 @@
 #!/bin/bash
 # Name: user-env-script.sh
-# Purpose: Get all normal users type and environment variables
+# Purpose: Get all users shell and environment variables
 # Author: Mohammed Tanvirul Islam
 ################################################################
+
 _login="/etc/login.defs"
 _passwd="/etc/passwd"
 
-## get mini UID limit ##
+## get min UID limit ##
 min_uid=$(grep "^UID_MIN" $_login)
 
 ## get max UID limit ##
 max_uid=$(grep "^UID_MAX" $_login)
 
-#File name
-file="exported_user_env_$(hostname)_"`date +"%d-%m-%Y"`.txt
-#Delete existing file
-if [ -f "$file" ]
-then
-    rm -f ./$file
-fi
+## Files name ##
+file_excel="users_env_$(hostname).xls"
+file_txt="users_env_$(hostname).txt"
+file_csv="users_env_$(hostname).csv"
 
-#write header
-#echo "user,type,variables" >> ./$file
+## Folder name ##
+folder_name="exported_users_env_$(hostname)_$(date +"%d-%m-%Y")"
+
+## Create folder ##
+if [ -d "$folder_name" ]
+then
+  rm -rf ./$folder_name
+  mkdir ./$folder_name
+else
+  mkdir ./$folder_name
+fi
+echo -e '\n'"Folder created: " $(pwd)/$folder_name'\n'
+
+## write header to excel and csv ##
+#echo -e "user" '\t' "shell" '\t' "variables" >> ./$folder_name/$file_txt
+echo -e "user" '\t' "shell" '\t' "variables" >> ./$folder_name/$file_excel
+echo -e "user,shell,variables" >> ./$folder_name/$file_csv
+
 ## use awk to print if UID >= $MIN and UID <= $MAX and shell is not /sbin/nologin   ##
 for user in $(awk -F':' -v "min=${min_uid##UID_MIN}" -v "max=${max_uid##UID_MAX}" '{ if ( $3 >= min && $3 <= max  && $7 != "/sbin/nologin" ) print $1 }' "$_passwd"); do
-  #user_type=$(getent passwd $user | awk -F':' '{ print $NF}')
-  echo $user " >> " ./$file
-  #echo "$(echo $user)","$(echo $user_type)","$(su - $user -c env | paste -sd "\n")" >> ./$file
-  #echo ""
-  echo "------------------------------[ $user : $(getent passwd $user | awk -F':' '{ print $NF}') ]------------------------------">> ./$file
-  su - $user -c env >> ./$file
-  echo "-------------------------------------[ FIN ]-------------------------------------" >> ./$file
-  echo "">> ./$file
+
+  echo $user " >> " $file_txt, $file_excel, $file_csv
+  user_shell=$(getent passwd $user | awk -F':' '{ print $NF}')
+
+  ## Write to text file ##
+  echo "------------------------------[ $user : $user_shell ]------------------------------">> ./$folder_name/$file_txt
+  su - $user -c env >> ./$folder_name/$file_txt
+  echo "---------------------------------------[ FIN ]--------------------------------------" >> ./$folder_name/$file_txt
+  echo -e '\n\n' >> ./$folder_name/$file_txt
+
+  ## Write to excel file ##
+  write_count=1
+  for user_env in $(su - $user -c env); do
+    if [ $write_count == 1 ]; then
+      echo -e $user '\t' $user_shell '\t' $user_env >> ./$folder_name/$file_excel
+    else
+      echo -e "" '\t' "" '\t' $user_env >> ./$folder_name/$file_excel
+    fi
+    write_count=`expr $write_count + 1`
+  done
+  echo -e '\n\n' >> ./$folder_name/$file_excel
+
+  ## Write to csv file ##
+  echo "$(echo $user)","$(echo $user_shell)","$(su - $user -c env | paste -sd "$")" >> ./$folder_name/$file_csv
+
 done
